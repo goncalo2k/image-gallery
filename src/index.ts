daffe5258575b2abc2bab077be546f63f127a6d0
Gonçalo Miranda <goncalo.miranda.cplx@gmail.com>
2026-03-10 17:57:56 +0000

    Added initial worker bindings for r2 access

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  ANALOGS_BUCKET: R2Bucket
+}
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/:imageName', async (c) => {
+  const key = c.req.param('imageName')
+
+  const object = await c.env.ANALOGS_BUCKET.get(key)
+
+  if (!object) {
+    return c.notFound()
+  }
+
+  return new Response(object.body)
+})
+
+app.get('/audit', (c) => {
+  return c.text('Got audit!')
 })
 
 export default app
